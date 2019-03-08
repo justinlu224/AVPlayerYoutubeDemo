d0b277faaaa0d19afd573091ca091c66c10ea2cf
justinlu224 <justinlu224@outlook.com>
2019-03-08 15:46:50 +0800

    影片播放完成

--- AVPlayerYoutubeDemo/LikeYoutubeViewController.swift
+++ AVPlayerYoutubeDemo/LikeYoutubeViewController.swift
@@ -7,8 +7,11 @@
 //
 
 import UIKit
+import AVFoundation
 
 class LikeYoutubeViewController: UIViewController {
+    @IBOutlet weak var viewRectView: UIView!
+    @IBOutlet weak var infoContainerView: UIView!
     //畫面拉下後的最小寬度
     var minWidth:CGFloat{
         return UIScreen.main.bounds.size.width/4
@@ -33,14 +36,48 @@ class LikeYoutubeViewController: UIViewController {
     var startPosition:CGPoint = .zero
     //起始位移量
     var startOffSet:CGFloat = 0
-    
+    //目前位移量
     var currentOffSet:CGFloat = 0
+    
+    var tap:UITapGestureRecognizer!
+    var player:AVPlayer = AVPlayer()
+    var playerLayer:CALayer!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // 在view上加入拖動手勢，上下拉動效果
-        self.view.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: #selector(panHandler(_ :))))
+        view.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: #selector(panHandler(_ :))))
+        //加入點擊手勢
+        tap = UITapGestureRecognizer(target: self, action: #selector(tap(_ :)))
+        tap.isEnabled = false
+        view.addGestureRecognizer(tap)
+    
+        let url = URL(string: "https://content.jwplatform.com/manifests/vM7nH0Kl.m3u8")
+        let playerItem = AVPlayerItem(url: url!)
+        //更換當前播放項目
+        player.replaceCurrentItem(with: playerItem)
+        playerLayer = AVPlayerLayer(player: player)
+        //如果影片沒有佔滿整個畫面，讓背景為黑色較佳
+        playerLayer.backgroundColor = UIColor.black.cgColor
+        //將layer加入
+        self.view.layer.addSublayer(playerLayer)
+      
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        playerLayer.frame = viewRectView.frame
+        player.play()
+    }
+    
+    
+    //點擊畫面放到最大
+    @objc
+    func tap(_ tap:UITapGestureRecognizer){
+        toMax()
     }
     //上下拉動效果
     @objc
@@ -74,7 +111,13 @@ class LikeYoutubeViewController: UIViewController {
             
             let newX = origWidth - newWidth
             self.view.frame = CGRect(origin: CGPoint(x: newX, y: newY), size: CGSize(width: newWidth, height: newHeight))
-            
+           
+            CATransaction.begin()
+            CATransaction.setDisableActions(true)
+            //讓影片在拖動畫面時會跟著畫面一起縮放
+            playerLayer.frame = CGRect(origin: .zero, size: CGSize(width: newWidth, height: newWidth / 16*9))
+            CATransaction.commit()
+
             break
         case .ended:
             //如果移動後的位置大於原來高度的1/3,讓畫面縮到最小 反之最大
@@ -91,6 +134,7 @@ class LikeYoutubeViewController: UIViewController {
     }
     
     func toMin(){
+        tap.isEnabled = true
         let x = origWidth - minWidth
         let y = origHeight - minHeight
         
@@ -98,14 +142,16 @@ class LikeYoutubeViewController: UIViewController {
             self.view.frame = CGRect(x: x, y: y, width: self.minWidth, height: self.minHeight)
             self.view.layoutIfNeeded()
         }
+         playerLayer.frame = viewRectView.frame
         self.startOffSet = maxOffSet
     }
     func toMax(){
-        
+        tap.isEnabled = false
         UIView.animate(withDuration: 1) {
             self.view.frame = CGRect(origin: .zero, size: CGSize(width: self.origWidth, height: self.origHeight))
                         self.view.layoutIfNeeded()
         }
+         playerLayer.frame = viewRectView.frame
         self.startOffSet = 0
     }
 
